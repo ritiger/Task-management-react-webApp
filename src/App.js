bcdff91188a975ad8e6038e8585ba05a05eaa2cc
ritiger <jinzhexing26@gmail.com>
2022-06-23 02:37:15 +0800

    finished CRUD operation

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import { useState } from "react";
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
+import AddTask from "./components/AddTask";
 
 const App = () => {
+  const [showAddTask, setShowAddTask] = useState(false);
   const [tasks, setTasks] = useState([
     {
       id: 1,
@@ -24,9 +26,20 @@ const App = () => {
     },
   ]);
 
+  //Add Task
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 10000 + 1);
+
+    const newTask = { id, ...task };
+
+    setTasks([...tasks, newTask]);
+  };
+
   // Delete Task
   const deleteTask = (id) => {
-    setTasks(tasks.filter((task) => task.id !== id));
+    if (window.confirm("Are you sure to delete a task?") === true) {
+      setTasks(tasks.filter((task) => task.id !== id));
+    }
   };
 
   // Toggle reminder
@@ -40,7 +53,11 @@ const App = () => {
 
   return (
     <div className="container">
-      <Header />
+      <Header
+        onShow={() => setShowAddTask(!showAddTask)}
+        showAddTask={showAddTask}
+      />
+      {showAddTask && <AddTask onAdd={addTask} />}
       {tasks.length > 0 ? (
         <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
       ) : (
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,15 @@
 import PropTypes from "prop-types";
 import Button from "./Button";
 
-const Header = ({ title }) => {
-  const onClick = () => {
-    console.log("click");
-  };
-
+const Header = ({ title, onShow, showAddTask }) => {
   return (
     <header className="header">
       <h1>{title}</h1>
-      <Button color="green" text="Add" onClick={onClick} />
+      <Button
+        color={showAddTask ? "red" : "green"}
+        text={showAddTask ? "Close" : "Add"}
+        onClick={onShow}
+      />
     </header>
   );
 };
